98056cdd1043fb982f796d67e1f0ee3c63fc4b38
ntycrow <ccookf@gmail.com>
2016-03-08 13:36:20 -0500

    Naive login implemented.  Setup basic login/logout routes with a session and jade views.

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Authentication and session store includes, setup session store
+var passport = require('passport');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+var store = new MongoDBStore(
+{
+    uri: 'mongodb://localhost:27017/notes',
+    collection: 'sessions'
+});
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -19,11 +28,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Passport authorization middleware
+var hour = 1000 * 60 * 60;
+app.use(session({
+    resave: true,
+    saveUnitialized: false,
+    secret: 'NoteToSelfGetAProperSecretGeneratorInHereLater', //Kind of pointless on a public repo
+    cookie: { httpOnly: true, secure: false, maxAge: hour },  //Will not use https on this project
+    store: store,
+    rolling: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+var store = new MongoDBStore(
+{
+    uri: 'mongodb://localhost:27017/notes',
+    collection: 'sessions'
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done)
+    {
+        //Placeholder for db search and password verification
+        var user = 
+        {
+            username: username,
+        };
+        
+        return done(null, user);
+    }
+));
+
+//@todo setup id serialization, this is just a filler passthrough
+passport.serializeUser(function(user,callback)
+{
+    callback(null, user);
+});
+
+passport.deserializeUser(function(user, callback)
+{
+    callback(null, user);
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) 
+{
+    var name;
+    try
+    {
+        name = req.user.username;
+    }
+    catch (e)
+    {
+        name = 'to the site';
+    }
+    res.render('index', { title: 'Notes', name: name });
+});
+
+router.get('/login', function(req, res)
+{
+    res.render('login');
+});
+
+router.get('/logout', function(req, res)
+{
+    req.session.destroy(function(err)
+    {
+        console.error('Failed to destroy session: %s', err);
+        res.redirect('/');
+    });
+});
+
+router.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), function(req, res)
+{
+    res.redirect('/'); 
 });
 
 module.exports = router;
